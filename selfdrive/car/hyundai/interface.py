7d0f5d71132f5a590f21230f6afcccb5fd1428e5
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2021-12-13 14:03:11 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -41,14 +41,14 @@ class CarInterface(CarInterfaceBase):
     ret.communityFeature = True
 
     tire_stiffness_factor = 1.
-    ret.maxSteeringAngleDeg = 1000.
+    ret.maxSteeringAngleDeg = 90.
 
     # lateral
     ret.lateralTuning.init('lqr')
 
-    ret.lateralTuning.lqr.scale = 1650.
+    ret.lateralTuning.lqr.scale = 1700.
     ret.lateralTuning.lqr.ki = 0.01
-    ret.lateralTuning.lqr.dcGain = 0.0027
+    ret.lateralTuning.lqr.dcGain = 0.0028
 
     ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
     ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
@@ -56,8 +56,8 @@ class CarInterface(CarInterfaceBase):
     ret.lateralTuning.lqr.k = [-110., 451.]
     ret.lateralTuning.lqr.l = [0.33, 0.318]
 
-    ret.steerRatio = 16.5
-    ret.steerActuatorDelay = 0.05
+    ret.steerRatio = 13.5
+    ret.steerActuatorDelay = 0.0
     ret.steerLimitTimer = 2.5
     ret.steerRateCost = 0.4
     ret.steerMaxBP = [0.]
