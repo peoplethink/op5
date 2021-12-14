5c2802b03374e92a61093651602349848891b7f0
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2021-12-14 20:20:56 +0900

    Update params.cc

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -190,7 +190,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"NewRadarInterface", PERSISTENT},
     {"DisableOpFcw", PERSISTENT},
     {"ShowDebugUI", PERSISTENT},
-
+    {"ShowBsdUI", PERSISTENT},
     {"C3MdpsSet", PERSISTENT},
     {"KeepSteeringTurnSignals", PERSISTENT},
     {"WarningOverSpeedLimit", PERSISTENT},
