944c333d656774433d0401599328f48e7dadd10e
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2021-12-14 19:58:25 +0900

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -964,23 +964,23 @@ void OnroadHud::drawCgear(QPainter &p, UIState& s) {
   QString tgear, tgearshifter;
 
   tgear.sprintf("%.0f", t_gear);
-  configFont(p, "Open Sans", 170, "Regular");
+  configFont(p, "Open Sans", 150, "Regular");
 
   shifter = 1; //디버그용
-  p.setPen(QColor(255, 165, 0, 255)); 
+  p.setPen(QColor(255, 255, 255, 255)); 
 
-  int x_gear = 85;
-  int y_gear = 800;
+  int x_gear = 85-50;
+  int y_gear = 800+100;
   if ((t_gear < 9) && (t_gear !=0)) { 
     p.drawText(x_gear, y_gear, tgear);
   } else if (t_gear == 14 ) { 
     p.setPen(QColor(201, 34, 49, 255));
     p.drawText(x_gear, y_gear, "R");
   } else if (shifter == 1 ) { 
-    p.setPen(QColor(0, 255, 0, 255));
+    p.setPen(QColor(255, 255, 255, 255));
     p.drawText(x_gear, y_gear, "P");
   } else if (shifter == 3 ) {  
-    p.setPen(QColor(0, 0, 255, 255));
+    p.setPen(QColor(255, 255, 255, 255));
     p.drawText(x_gear, y_gear, "N");
   }
   // 1 "P"   2 "D"  3 "N" 4 "R"
