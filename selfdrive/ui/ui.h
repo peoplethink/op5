5d170e8649e70128a6a5a7289b1f8e65e86c836d
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2021-12-14 20:26:29 +0900

    Update ui.h

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -125,10 +125,6 @@ typedef struct UIState {
   bool show_debug = false;
   bool show_gear = false;//기어
   bool show_bsd = false;//bsd
-  bool leftblindspot;
-  bool rightblindspot;
-  int blindspot_blinkingrate = 120;
-  int car_valid_status_changed = 0;
 
 } UIState;
 
