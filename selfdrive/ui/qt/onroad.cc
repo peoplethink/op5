5a4102178b848fd4191599af7407a486b6179167
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2021-12-15 21:33:07 +0900

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -566,6 +566,23 @@ void OnroadHud::drawSpeed(QPainter &p, UIState& s) {
   drawText(p, rect().center().x(), 230, speed);
   configFont(p, "Open Sans", 66, "Regular");
   //drawText(p, rect().center().x(), 310, s.scene.is_metric ? "km/h" : "mph", 200);
+	
+  // update engageability and DM icons at 2Hz
+  if (sm.frame % (UI_FREQ / 2) == 0) {
+    setProperty("engageable", cs.getEngageable() || cs.getEnabled());
+    //setProperty("dmActive", sm["driverMonitoringState"].getDriverMonitoringState().getIsActiveMode());
+  }
+}
+
+void OnroadHud::paintEvent(QPaintEvent *event) {
+  QPainter p(this);
+  p.setRenderHint(QPainter::Antialiasing);
+	
+  // engage-ability icon
+  if (engageable) {
+    drawIcon(p, rect().right() - radius / 2 - bdr_s * 2, radius / 2 + int(bdr_s * 1.5),
+             engage_img, bg_colors[status], 1.0);
+  }
 }
 
 static const QColor get_tpms_color(float tpms) {
