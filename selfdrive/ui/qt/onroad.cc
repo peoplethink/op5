578558c7091b62dd6b2b4cbf77dc8c7e06396dcc
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2021-12-15 21:05:52 +0900

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -970,7 +970,7 @@ void OnroadHud::drawCgear(QPainter &p, UIState& s) {
   tgear.sprintf("%.0f", t_gear);
   configFont(p, "Open Sans", 150, "Semi Bold");
 
-  shifter = 1; //디버그용
+  //shifter = 1; //디버그용
   p.setPen(QColor(255, 255, 255, 255)); 
 
   int x_gear = 45;
