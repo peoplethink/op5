ac7d2b77f80fd6057f9a287e82673e30043d9297
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2021-12-21 06:34:03 +0900

    Update onroad.h

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -69,6 +69,7 @@ private:
   void drawCgear(QPainter &p, UIState& s);//기어
   void drawBsd(QPainter &p, UIState& s);//bsd
   void drawTpms(QPainter &p, UIState& s);
+  void drawBrakeStatus(QPainter &p, UIState& s);
   
 public:
   void drawCommunity(QPainter &p, UIState& s);
