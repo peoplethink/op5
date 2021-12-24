2906f6f86db52572c96af8299bf60808e8ec4bc3
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2021-12-24 22:25:48 +0900

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -300,9 +300,9 @@ void OnroadHud::paintEvent(QPaintEvent *event) {
              engage_img, bg_colors[status], 5.0, true, ang_str );
   }
 
-  if(uiState().recording) {
+  if(uiState()->recording) {
     QPainter p(this);
-    drawCommunity(p, uiState());
+    drawCommunity(p);
   }
 }
 
@@ -441,7 +441,7 @@ void NvgWindow::paintGL() {
   if (s->scene.world_objects_visible) {
     if(!s->recording) {
       QPainter p(this);
-      hud->drawCommunity(p, uiState());
+      hud->drawCommunity(p);
     }
     else {
       hud->update();
