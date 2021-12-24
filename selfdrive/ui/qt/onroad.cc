ff387155d6df663523629e0eb90f9b5c91b0cd61
marine2875 <79120100+marine2875@users.noreply.github.com>
2021-12-24 23:54:22 +0900

    Revert "Update onroad.cc"
    This reverts commit e6945d54c356e965afcb5f88dd62afeb70dd9118.

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -302,7 +302,7 @@ void OnroadHud::paintEvent(QPaintEvent *event) {
 
   if(uiState()->recording) {
     QPainter p(this);
-    drawCommunity(p, UIState& s);
+    drawCommunity(p, s);
   }
 }
 
@@ -441,7 +441,7 @@ void NvgWindow::paintGL() {
   if (s->scene.world_objects_visible) {
     if(!s->recording) {
       QPainter p(this);
-      hud->drawCommunity(p, UIState& s);
+      hud->drawCommunity(p, s);
     }
   }
 
