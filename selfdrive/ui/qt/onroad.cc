05f00bc084ce5640dff786834f07b2aa76e7a52d
marine2875 <79120100+marine2875@users.noreply.github.com>
2021-12-28 19:05:49 +0900

    paint path hue

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -384,11 +384,22 @@ void NvgWindow::updateFrameMat(int w, int h) {
       .translate(-intrinsic_matrix.v[2], -intrinsic_matrix.v[5]);
 }
 
-void OnroadHud::drawLaneLines(QPainter &painter, const UIScene &scene) {
+void OnroadHud::drawLaneLines(QPainter &painter, UIState *s) {
+  UIScene &scene = s->scene;
   if (!scene.end_to_end) {
     // lanelines
     for (int i = 0; i < std::size(scene.lane_line_vertices); ++i) {
-      painter.setBrush(QColor::fromRgbF(0, 1.0, 0, scene.lane_line_probs[i]));
+      if (i == 1 || i == 2) {
+        // TODO: can we just use the projected vertices somehow?
+        const cereal::ModelDataV2::XYZTData::Reader &line = (*s->sm)["modelV2"].getModelV2().getLaneLines()[i];
+        const float default_pos = 1.4;  // when lane poly isn't available
+        const float lane_pos = line.getY().size() > 0 ? std::abs(line.getY()[5]) : default_pos;  // get redder when line is closer to car
+        float hue = 332.5 * lane_pos - 332.5;  // equivalent to {1.4, 1.0}: {133, 0} (green to red)
+        hue = std::fmin(133, fmax(0, hue)) / 360.;  // clip and normalize
+        painter.setBrush(QColor::fromHslF(hue, 0.73, 0.64, scene.lane_line_probs[i]));
+      } else {
+        painter.setBrush(QColor::fromRgbF(1.0, 1.0, 1.0, scene.lane_line_probs[i]));
+      }
       painter.drawPolygon(scene.lane_line_vertices[i].v, scene.lane_line_vertices[i].cnt);
     }
     // road edges
@@ -397,10 +408,21 @@ void OnroadHud::drawLaneLines(QPainter &painter, const UIScene &scene) {
       painter.drawPolygon(scene.road_edge_vertices[i].v, scene.road_edge_vertices[i].cnt);
     }
   }
+	
   // paint path
   QLinearGradient bg(0, height(), 0, height() / 4);
-  bg.setColorAt(0, scene.end_to_end ? redColor(200) : QColor(135, 206, 235, 200));
-  bg.setColorAt(1, scene.end_to_end ? redColor(0) : QColor(255, 255, 255, 0));
+  if ((*s->sm)["controlsState"].getControlsState().getEnabled()) {
+    const cereal::ModelDataV2::XYZTData::Reader &pos = (*s->sm)["modelV2"].getModelV2().getPosition();
+    const float lat_pos = pos.getY().size() > 0 ? std::abs(pos.getY()[14] - pos.getY()[0]) : 0;  // 14 is 1.91406 (subtract initial pos to not consider offset)
+    float hue = lat_pos * -39.46 + 148;  // interp from {0, 4.5} -> {148, 0}  
+    hue = fmod(hue, 360.0) / 360.0;  // scale and wrap around
+    bg.setColorAt(0, QColor::fromHslF(hue, .94, .51, 1.));
+    bg.setColorAt(1, QColor::fromHslF(hue, .73, .49, 100./255.));
+  } else {
+    bg.setColorAt(0, scene.end_to_end ? redColor() : QColor(255, 255, 255));
+    bg.setColorAt(1, scene.end_to_end ? redColor(0) : QColor(255, 255, 255, 0));
+  }
+
   painter.setBrush(bg);
   painter.drawPolygon(scene.track_vertices.v, scene.track_vertices.cnt);
 }
@@ -480,7 +502,7 @@ void OnroadHud::drawCommunity(QPainter &p) {
 
   const SubMaster &sm = *(s->sm);
 
-  drawLaneLines(p, s->scene);
+  drawLaneLines(p, s);
 
   auto leads = sm["modelV2"].getModelV2().getLeadsV3();
   if (leads[0].getProb() > .5) {
--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -55,7 +55,7 @@ private:
   QPixmap ic_lcr;
 
   inline QColor redColor(int alpha = 255) { return QColor(201, 34, 49, alpha); }
-  void drawLaneLines(QPainter &painter, const UIScene &scene);
+  void drawLaneLines(QPainter &painter, UIState *s);
   void drawLead(QPainter &painter, const cereal::ModelDataV2::LeadDataV3::Reader &lead_data, const QPointF &vd, bool is_radar);
 
   void drawText2(QPainter &p, int x, int y, int flags, const QString &text, const QColor& color);
