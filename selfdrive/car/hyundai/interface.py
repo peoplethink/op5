e76ddaa8e75a87fc849fbad8a338582ec2ec30d6
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2021-12-29 19:23:38 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -121,8 +121,8 @@ class CarInterface(CarInterfaceBase):
     ret.stopAccel = -2.0
     ret.startingAccelRate = 5.0  # brake_travel/s while releasing on restart
     ret.stoppingDecelRate = 0.6  # brake_travel/s while trying to stop
-    ret.vEgoStopping = 0.6
-    ret.vEgoStarting = 0.5
+    #ret.vEgoStopping = 0.6
+    #ret.vEgoStarting = 0.5
 
     # genesis
     if candidate == CAR.GENESIS:
