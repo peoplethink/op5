47c93c7d9750eb46ddd97897a1cfae2f7f61c379
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2021-12-31 07:29:59 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -50,13 +50,13 @@ class CarInterface(CarInterfaceBase):
     # -------------PID
     if Params().get("LateralControlSelect", encoding='utf8') == "0":
       if candidate in [CAR.GENESIS, CAR.GENESIS_G80, CAR.GENESIS_EQ900]:
-          ret.lateralTuning.pid.kf = 0.00006
+          ret.lateralTuning.pid.kf = 0.000058
           ret.lateralTuning.pid.kpBP = [0., 10., 30.]
           ret.lateralTuning.pid.kpV = [0.01, 0.05, 0.065]
           ret.lateralTuning.pid.kiBP = [0., 30.]
           ret.lateralTuning.pid.kiV = [0.02, 0.02]
           ret.lateralTuning.pid.kdBP = [0.]
-          ret.lateralTuning.pid.kdV = [0.1]
+          ret.lateralTuning.pid.kdV = [1.0]
           ret.lateralTuning.pid.newKfTuned = True
           
           #ret.lateralTuning.pid.kf = 0.00003
