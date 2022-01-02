3d70e61d7b56aab82f8280ae3563608d2a369c51
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-01-02 11:41:14 +0900

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -35,7 +35,7 @@ X_EGO_COST = 0.
 V_EGO_COST = 0.
 A_EGO_COST = 0.
 J_EGO_COST = 5.0
-A_CHANGE_COST = .05
+A_CHANGE_COST = .02
 DANGER_ZONE_COST = 100.
 CRASH_DISTANCE = .5
 LIMIT_COST = 1e6
@@ -45,7 +45,7 @@ CRUISE_GAP_BP = [1., 2., 3., 4.]
 CRUISE_GAP_V = [1.2, 1.35, 1.5, 1.7]
 
 AUTO_TR_BP = [0., 10.*CV.KPH_TO_MS, 70.*CV.KPH_TO_MS, 110.*CV.KPH_TO_MS]
-AUTO_TR_V = [1., 1.2, 1.35, 1.45]
+AUTO_TR_V = [1., 1.1, 1.35, 1.45]
 
 AUTO_TR_CRUISE_GAP = 4
 
@@ -60,7 +60,7 @@ T_IDXS_LST = [index_function(idx, max_val=MAX_T, max_idx=N+1) for idx in range(N
 T_IDXS = np.array(T_IDXS_LST)
 T_DIFFS = np.diff(T_IDXS, prepend=[0.])
 MIN_ACCEL = -3.5
-T_FOLLOW = 1.15
+T_FOLLOW = 1.05
 COMFORT_BRAKE = 2.5
 STOP_DISTANCE = 7.0
 
